02655ca0d8c22d1df72442b8f23b75e9cdb29a6b
Renzy Alarcon <Renzy.Alarcon@hotmail.com>
2021-12-21 09:13:24 -0500

    - Added DX11's common include

---
+++ Projects/Insanity_Engine_Rendering/Rendering/Backends/DX11/Common_Include.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <d3d11_4.h>
+#include <d3dcompiler.h>
+#include <dxgi1_6.h>
+#include <wrl/client.h>
+
+
+namespace InsanityEngine::Rendering::DX11
+{
+    using Microsoft::WRL::ComPtr;
+}
