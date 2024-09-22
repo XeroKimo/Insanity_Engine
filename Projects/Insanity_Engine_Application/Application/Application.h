1e5dc8ee65151be110bff8ab8ccc7280225b3e89
Renzy Alarcon <Renzy.Alarcon@hotmail.com>
2024-09-21 23:31:25 -0400

    - Initial Commit

--- Projects/Insanity_Engine_Application/Application/Application.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "ApplicationSettings.h"
-
-namespace InsanityEngine::Application
-{
-    extern int Run(const Settings& settings);
-}
--- Projects/Insanity_Engine_Application/Rendering/Device.cpp
+++
@@ -1,9 +0,0 @@
-#include "Device.h"
-
-namespace InsanityEngine::Rendering
-{
-    Device::DX12::DX12() :
-        m_device(TypedD3D::D3D12::CreateDevice<TypedD3D::D3D12::Device5>(D3D_FEATURE_LEVEL_12_0, nullptr).value())
-    {
-    }
-}
